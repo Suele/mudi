fa58217ed064e492a504cfb444f9bf81228740de
Suele <suelen.ads.guimaraes@gmail.com>
2021-12-14 20:26:20 -0300

    Adiciona as anotações da JPA para fazer o mapeamento entre a tabela do banco de dados e a classe java para acesso aos dados.

--- src/main/java/com/world/mudi/model/ProductRequestModel.java
+++ src/main/java/com/world/mudi/model/ProductRequestModel.java
@@ -1,18 +1,36 @@
 package com.world.mudi.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity(name = "ProductRequestEntity")
+@Table(name = "product_request")
 public class ProductRequestModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "product_name")
 	private String productName;
+
 	private Double price;
+
+	@Column(name = "delivery_date")
 	private LocalDate deliveryDate;
+
 	private String description;
+
+	@Column(name = "url_product")
 	private String urlProduct;
+
+	@Column(name = "url_image")
 	private String urlImage;
 
-	public ProductRequestModel(String productName, LocalDate deliveryDate, Double price, String description,
-							   String urlProduct, String urlImage) {
+	public ProductRequestModel() {
+	}
+
+	public ProductRequestModel(String productName, LocalDate deliveryDate,
+							   Double price, String description, String urlProduct, String urlImage) {
 		this.productName = productName;
 		this.price = price;
 		this.deliveryDate = deliveryDate;
