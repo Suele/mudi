21d12a88f7448815704a3a5c011a8fe963008b9a
Suele <suelen.ads.guimaraes@gmail.com>
2021-12-14 21:48:48 -0300

    Interface para ter acesso aos metodos de consulta e persistencia do BD.

---
+++ src/main/java/com/world/mudi/repository/ProductRequestRepository.java
@@ -0,0 +1,19 @@
+package com.world.mudi.repository;
+
+import com.world.mudi.model.ProductRequestModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author Suele Guimaraes
+ * Como está interface extende outra interface que tem diversos metodos para consultar e persistir dados
+ * alguns metodos não são necessarios serem implementados.
+ */
+
+@Repository
+public interface ProductRequestRepository extends JpaRepository<ProductRequestModel, Long> {
+}
