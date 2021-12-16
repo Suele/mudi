8c054ab6138ef20eb0c4f5d44bd2daf29bc47c6d
Suele <suelen.ads.guimaraes@gmail.com>
2021-12-16 18:16:16 -0300

    Ajusta o acesso da classe HomeController para a classe service.

--- src/main/java/com/world/mudi/controller/HomeController.java
+++ src/main/java/com/world/mudi/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.world.mudi.controller;
 
 import com.world.mudi.model.ProductRequestModel;
 import com.world.mudi.repository.ProductRequestRepository;
+import com.world.mudi.service.ProductRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,11 +17,11 @@ import java.util.List;
 @Controller
 public class HomeController {
 	@Autowired
-	private ProductRequestRepository productRequestRepository;
+	private ProductRequestService productRequestService;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		List<ProductRequestModel> listProductRequest = productRequestRepository.findAll();
+		List<ProductRequestModel> listProductRequest = productRequestService.listProducts();
 		model.addAttribute("listProductRequest", listProductRequest);
 		return "home";
 	}
