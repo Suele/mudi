60e07e10a4420446e33d68ffe76c990d1674d5e0
Suele <suelen.ads.guimaraes@gmail.com>
2022-01-12 23:35:20 -0300

    Adiciona a criptografia de senha e o acesso ao banco de dados para a verificação da existencia ou não do usuario.

--- src/main/java/com/world/mudi/WebSecurityConfig.java
+++ src/main/java/com/world/mudi/WebSecurityConfig.java
@@ -1,18 +1,24 @@
 package com.world.mudi;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -21,20 +27,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.formLogin()
 				.loginPage("/login")
+				.defaultSuccessUrl("/home", true)
 				.permitAll()
 				.and().logout(logout -> logout.logoutUrl("/logout"));
 	}
 
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 		UserDetails user =
-				User.withDefaultPasswordEncoder()
+				User.builder()
 						.username("user")
-						.password("password")
+						.password(encoder.encode("user"))
 						.roles("USER")
 						.build();
 
-		return new InMemoryUserDetailsManager(user);
+		auth.jdbcAuthentication()
+				.dataSource(dataSource)
+				.passwordEncoder(encoder)
+				.withUser(user);
 	}
 }
