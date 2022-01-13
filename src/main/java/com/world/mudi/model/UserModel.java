942c0e90ff590c4f79b70570b19d4fd9bd3d5e9b
Suele <suelen.ads.guimaraes@gmail.com>
2022-01-12 23:33:10 -0300

    Adiciona a entidade que representa a tabela de usuarios para autenticação no spring security.

---
+++ src/main/java/com/world/mudi/model/UserModel.java
@@ -0,0 +1,53 @@
+package com.world.mudi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel {
+	@Id
+	@Column(nullable = false)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private Boolean enabled;
+
+	public UserModel() {
+	}
+
+	public UserModel(String username, String password, Boolean enabled) {
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+}
